da976cafd1514c5c10b68a2e594d3c38a6966b9f
Carl Ramsell <carlramsell@Carls-Mac-mini.local>
2015-10-18 13:22:54 +0200

    update image

--- about.php
+++ about.php
@@ -11,7 +11,7 @@
 
 <body>
 
-<div id='wrapper'>
+<div id='wrapper' class='hovergrid'>
 
 <div id='header'>
 <?php include('includes/theme-header.html'); ?>
@@ -22,47 +22,8 @@
 </div>
 
 <div id='wrap-main'>
-<div id='main'><article class="article1">
-<h1>About the Orbit theme</h1>
-<p>A template with regions for webpages/frameworks built with PHP.</p>
-
-<p><a href='https://github.com/helikopterspark/orbit-theme'>Get the theme on github</a></p>
-
-<p>Demo <a href='http://www.student.bth.se/~carb14/phpmvc/orbit-theme/index.php'>here</a></p>
-<hr>
-
-<p>Based on The Semantic Grid System.</p>
-
-<p>Remember to give write rights to the css folder in order for stylephp to create the css file: <code>chmod 777 css</code></p>
- 
-<p>Built by Carl Ramsell as a part of the phpmvc course at Blekinge Tekniska Högskola.</p>
-
-<p>Photos from the NASA Image Gallery.</p>
-
-<hr>
- 
-<h3>License</h3>
- 
-<p>This software is free software and carries a MIT license.</p>
-
-<hr>
-
-<h3>Use of external libraries</h3>
-
-<p>The following external modules are included and subject to its own license.</p>
-
-<h4>lessphp</h4>
-<p>with function autocompile by mosbth<br/>
-* Website: <a href='https://github.com/mosbth/stylephp'>https://github.com/mosbth/stylephp</a></p>
-
-<h4>Font-Awesome</h4>
-<p>* Website: <a href='http://fortawesome.github.io/Font-Awesome/'>http://fortawesome.github.io/Font-Awesome/</a></p>
-
-<h4>The Semantic Grid System</h4>
-<p>* Website: <a href='http://tylertate.github.io/semantic.gs/'>http://tylertate.github.io/semantic.gs/</a></p>
-</article>
-</div>
-<div id='sidebar'><article class="article1"><img src='img/blastoff.jpg' alt='Rocket launch' width='275px;'/></article></div>
+<div id='main'><?php include('includes/about.html'); ?></div>
+<div id='sidebar'><article class="article1"><img src='img/blastoff.jpg' alt='Rocket launch'/></article></div>
 </div>
 
 <div id='footer'>
